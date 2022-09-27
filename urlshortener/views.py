1ef955cf4679b3aaf4872c4d6ae2f327e0c23803
manoj-gaonkar <manojgaonkar7648@gmail.com>
2022-09-27 16:06:42 +0530

    initial commit

---
+++ urlshortener/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from pyshorteners import Shortener
+
+# Create your views here.
+def index(request):
+    
+    if request.method == "POST":
+        longurl = request.POST['longurl']
+        tiny = Shortener()
+        shorturl = tiny.tinyurl.short(longurl)
+        print(shorturl)
+
+        context = { "shorturl": shorturl }
+        
+    else:
+        print("ok")
+        context = {}
+
+    return render(request, 'index.html',context)
